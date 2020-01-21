0e9afb5020d9666e6d198a0203a7406e394ec8f0
jblackvk <jvkouetchou@outlook.com>
2020-01-21 23:42:35 +0100

    correction du bugs avec la BD

--- src/app/Module/lieu/lieu.module.ts
+++ src/app/Module/lieu/lieu.module.ts
@@ -25,7 +25,32 @@ export class LieuModule {
     index.onsuccess = (e) => {
       // @ts-ignore
       const db = e.target.result;
-      const obj = db.transaction([this.variable.dbTable[2]], 'readwrite');
+      const obj = db.transaction([this.variable.dbTable[1]], 'readwrite');
+      const lieu = obj.objectStore().getAll();
+      lieu.onsuccess = () => {
+        alert('lieu enregistre');
+      };
     };
   }
+
+  setLieu(lieu: Lieu) {
+    const index = indexedDB.open(this.variable.dbname);
+    index.onsuccess = (e) => {
+      // @ts-ignore
+      const db = e.target.result;
+      const obj = db.transaction([this.variable.dbTable[1]], 'readwrite');
+      const store = obj.objectStore(this.variable.dbTable[1]);
+      store.add(lieu);
+    };
+  }
+}
+
+interface Lieu {
+  ville: string;
+  region: string;
+  pays: string;
+  coords: {
+    lat: number;
+    long: number;
+  };
 }
